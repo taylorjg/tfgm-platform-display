65d73580744998a38f2cb1b88ff3d2064371a53f
Jonathan Taylor <jonathan.taylor@disney.com>
2026-04-04 10:03:43 +0100

    Prepare for deployment to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "tfgm-platform-display",
   plugins: [react()],
   resolve: {
     alias: {
