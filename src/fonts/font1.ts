0c3dd15842cfc79748ffd2d2dfc4e7fc357b7028
Jonathan Taylor <jonathan.taylor@disney.com>
2026-05-14 23:28:56 +0100

    Update uppercase letters and symbols of font1

--- src/fonts/font1.ts
+++ src/fonts/font1.ts
@@ -3,26 +3,26 @@ import { makeFontMapKvps } from "./utils";
 // https://typofoto.wordpress.com/2014/06/19/dot-matrix-fonts/
 
 const uppercaseLetters = `
- xx  xxx   xx  xxx  xxxx xxxx  xx  x  x xxx  xxx x  x x    x   x x  x  xx  xxx   xx   xxx   xx  xxxxx x  x x   x x   x x   x x   x xxxx
-x  x x  x x  x x  x x    x    x  x x  x  x     x x  x x    xx xx xx x x  x x  x x  x  x  x x  x   x   x  x x   x x   x x   x x   x    x
-x  x x  x x    x  x x    x    x    x  x  x     x x x  x    x x x x xx x  x x  x x  x  x  x x      x   x  x x   x x   x  x x   x x    x 
-xxxx xxx  x    x  x xxx  xxx  x xx xxxx  x     x xx   x    x x x x  x x  x xxx  x  x  xxx   xx    x   x  x x   x x x x   x     x    x  
-x  x x  x x    x  x x    x    x  x x  x  x     x x x  x    x   x x  x x  x x    x xx  x x     x   x   x  x x   x x x x  x x    x   x   
-x  x x  x x  x x  x x    x    x  x x  x  x  x  x x  x x    x   x x  x x  x x    x  x  x  x x  x   x   x  x  x x  xx xx x   x   x   x   
-x  x xxx   xx  xxx  xxxx x     xx  x  x xxx  xx  x  x xxxx x   x x  x  xx  x     xx x x  x  xx    x    xx    x   x   x x   x   x   xxxx
-                                                                                                                                       
-    |    |    |    |    |    |    |    |   |    |    |    |     |    |    |    |     |    |    |     |    |     |     |     |     |    `;
+ xxx  xxxx   xxx  xxxx  xxxxx xxxxx  xxx  x   x xxx  xxxx x  x x     x   x x   x  xxx  xxxx   xxx   xxxx   xxx  xxxxx x   x x   x x   x x   x x   x xxxx
+x   x x   x x   x x   x x     x     x   x x   x  x      x x  x x     xx xx xx  x x   x x   x x   x  x   x x   x   x   x   x x   x x   x x   x x   x    x
+x   x x   x x     x   x x     x     x     x   x  x      x x x  x     x x x x x x x   x x   x x   x  x   x x       x   x   x x   x x   x  x x   x x    x 
+xxxxx xxxx  x     x   x xxxx  xxxx  x  xx xxxxx  x      x xx   x     x x x x  xx x   x xxxx  x   x  xxxx   xxx    x   x   x x   x x x x   x     x    x  
+x   x x   x x     x   x x     x     x   x x   x  x      x x x  x     x   x x   x x   x x     x  xx  x  x      x   x   x   x x   x x x x  x x    x   x   
+x   x x   x x   x x   x x     x     x   x x   x  x  x   x x  x x     x   x x   x x   x x     x   x  x   x x   x   x   x   x  x x  xx xx x   x   x   x   
+x   x xxxx   xxx  xxxx  xxxxx x      xxx  x   x xxx  xxx  x  x xxxxx x   x x   x  xxx  x      xxx x x   x  xxx    x    xxx    x   x   x x   x   x   xxxx
+                                                                                                                                                        
+     |     |     |     |     |     |     |     |   |     |    |     |     |     |     |     |      |     |     |     |     |     |     |     |     |    `;
 
 const lowercaseLetters = `
-      x               x         x        x      x      x    xx                                             x                                      
-      x               x        x x       x           x x     x                                             x                                      
- xxx  xxxx   xxx   xxxx  xxx   x    xxx  xxxx  xx      x  x  x  xx x  x xx   xxx  xxxx   xxxx x xx   xxxx xxx  x   x x   x x   x x   x x   x xxxxx
-    x x   x x   x x   x x   x xxx  x   x x   x  x    x x x   x  x x x xx  x x   x x   x x   x xx  x x      x   x   x x   x x   x  x x  x   x    x 
- xxxx x   x x     x   x xxxxx  x   x   x x   x  x    x xx    x  x x x x   x x   x x   x x   x x      xxx   x   x   x x   x x x x   x   x   x   x  
-x   x x   x x   x x   x x      x   x   x x   x  x    x x x   x  x   x x   x x   x x   x x   x x         x  x   x   x  x x  x x x  x x  x   x  x   
- xxxx xxxx   xxx   xxxx  xxxx  x    xxxx x   x xxx   x x  x xxx x   x x   x  xxx  xxxx   xxxx x     xxxx    xx  xxx    x    x x  x   x  xxxx xxxxx
-                                       x             x                            x         x                                              x      
-     |     |     |     |     |    |xxxx |     |   |xx |    |   |     |     |     |x    |    x|     |     |    |     |     |     |     |xxxx |     `;
+      x               x         x        x       x      x     xx                                             x                                      
+      x               x        x x       x            x x      x                                             x                                      
+ xxx  xxxx   xxx   xxxx  xxx   x    xxx  xxxx   xx      x  x   x  xx x  x xx   xxx  xxxx   xxxx x xx   xxxx xxx  x   x x   x x   x x   x x   x xxxxx
+    x x   x x   x x   x x   x xxx  x   x x   x   x    x x x    x  x x x xx  x x   x x   x x   x xx  x x      x   x   x x   x x   x  x x  x   x    x 
+ xxxx x   x x     x   x xxxxx  x   x   x x   x   x    x xx     x  x x x x   x x   x x   x x   x x      xxx   x   x   x x   x x x x   x   x   x   x  
+x   x x   x x   x x   x x      x   x   x x   x   x    x x x    x  x   x x   x x   x x   x x   x x         x  x   x   x  x x  x x x  x x  x   x  x   
+ xxxx xxxx   xxx   xxxx  xxxx  x    xxxx x   x  xxx   x x  x  xxx x   x x   x  xxx  xxxx   xxxx x     xxxx    xx  xxx    x    x x  x   x  xxxx xxxxx
+                                       x              x                             x         x                                              x      
+     |     |     |     |     |    |xxxx |     |    |xx |    |    |     |     |     |x    |    x|     |     |    |     |     |     |     |xxxx |     `;
 
 const numbers = `
  xxx    x   xxx   xxx     x  xxxxx  xxx  xxxxx  xxx   xxx 
@@ -36,20 +36,20 @@ x   x   x   x    x   x    x  x   x x   x   x   x   x x   x
      |    |     |     |     |     |     |     |     |     `;
 
 const symbols = `
-            
-            
-            
-         x  
-     xxx    
-         x  
-           x
-            
-    |   | | 
+                    x
+                    x
+                     
+           x         
+     xxxxx           
+           x         
+              x  x   
+                 x   
+    |     | |  |x |  
 `;
 
 export const fontMap1 = new Map([
   ...makeFontMapKvps("ABCDEFGHIJKLMNOPQRSTUVWXYZ", uppercaseLetters),
   ...makeFontMapKvps("abcdefghijklmnopqrstuvwxyz", lowercaseLetters),
   ...makeFontMapKvps("0123456789", numbers),
-  ...makeFontMapKvps(" -:.", symbols),
+  ...makeFontMapKvps(" -:.,'", symbols),
 ]);
