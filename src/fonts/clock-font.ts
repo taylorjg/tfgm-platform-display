9ac15f7aa05b81a2b82eaf0b83865a435332950d
Jonathan Taylor <jonathan.taylor@disney.com>
2026-05-23 10:04:24 +0100

    Tweak to clock font

--- src/fonts/clock-font.ts
+++ src/fonts/clock-font.ts
@@ -2,18 +2,31 @@ import { makeFontMapKvps } from "./utils";
 
 // https://typofoto.wordpress.com/2014/06/19/dot-matrix-fonts/
 
-const characters = `
- xxxxxx     xx     xxxxxx   xxxxxx      xxx  xxxxxxx   xxxxxx  xxxxxxxx  xxxxxx   xxxxxx         
-xx    xx   xxx    xx    xx xx    xx    xxxx  xx       xx    xx       xx xx    xx xx    xx  xx    
-xx    xx    xx          xx       xx   xx xx  xx       xx            xx  xx    xx xx    xx  xx    
-xx    xx    xx          xx       xx  xx  xx  xxxxxxx  xxxxxxx      xx   xx    xx xx    xx        
-xx    xx    xx     xxxxxx    xxxxx  xx   xx        xx xx    xx    xx     xxxxxx   xxxxxxx        
-xx    xx    xx    xx             xx xx   xx        xx xx    xx   xx     xx    xx       xx        
-xx    xx    xx    xx             xx xxxxxxxx       xx xx    xx   xx     xx    xx       xx  xx    
-xx    xx    xx    xx       xx    xx      xx  xx    xx xx    xx   xx     xx    xx xx    xx  xx    
- xxxxxx |  xxxx  |xxxxxxxx| xxxxxx |     xx | xxxxxx | xxxxxx |  xx    | xxxxxx | xxxxxx |   |   
+const numbers = `
+ xxxxxx     xx     xxxxxx   xxxxxx      xxx  xxxxxxx   xxxxxx  xxxxxxxx  xxxxxx   xxxxxx 
+xx    xx   xxx    xx    xx xx    xx    xxxx  xx       xx    xx       xx xx    xx xx    xx
+xx    xx    xx          xx       xx   xx xx  xx       xx            xx  xx    xx xx    xx
+xx    xx    xx          xx       xx  xx  xx  xxxxxxx  xxxxxxx      xx   xx    xx xx    xx
+xx    xx    xx     xxxxxx    xxxxx  xx   xx        xx xx    xx    xx     xxxxxx   xxxxxxx
+xx    xx    xx    xx             xx xx   xx        xx xx    xx   xx     xx    xx       xx
+xx    xx    xx    xx             xx xxxxxxxx       xx xx    xx   xx     xx    xx       xx
+xx    xx    xx    xx       xx    xx      xx  xx    xx xx    xx   xx     xx    xx xx    xx
+ xxxxxx |  xxxx  |xxxxxxxx| xxxxxx |     xx | xxxxxx | xxxxxx |  xx    | xxxxxx | xxxxxx 
+`;
+
+const symbols = `
+       
+     xx
+     xx
+       
+       
+       
+     xx
+     xx
+   |   
 `;
 
 export const clockFontMap = new Map([
-  ...makeFontMapKvps("0123456789: ", characters),
+  ...makeFontMapKvps("0123456789", numbers),
+  ...makeFontMapKvps(" :", symbols),
 ]);
